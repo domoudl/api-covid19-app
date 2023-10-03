ac70e249b2a9454f9247565e1a368ff91f714bea
domoudl <issaddl2000@proton.me>
2023-10-03 14:53:54 +0000

    Initial commit

---
+++ src/main/java/projetws/api/apicovid19app/ApiCovid19AppApplication.java
@@ -0,0 +1,13 @@
+package projetws.api.apicovid19app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCovid19AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCovid19AppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projetws/api/apicovid19app/ApiCovid19AppApplicationTests.java
@@ -0,0 +1,13 @@
+package projetws.api.apicovid19app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCovid19AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
